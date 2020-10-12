a914222c4d80b24dea8ec2bacd35b40da0257516
BelikSergey <65306747+BelikSergey@users.noreply.github.com>
2020-10-12 23:41:20 +0300

    Добавляет решение всей домашки.осталось доделать слайдер

--- gallery-items.js
+++ gallery-items.js
@@ -1,19 +1,40 @@
 import galleryData from "./export.js";
-console.log(galleryData);
+// const data = galleryData.forEach((element, inx) => {
+//   console.log(`${element}   ${inx}`);
+// });
+// console.log(data);
+
+let currentIndex = 0;
 
 // Создание и рендер разметки по массиву данных и предоставленному шаблону.
 const listGaleryItemsEl = document.querySelector(".js-gallery");
-console.log(listGaleryItemsEl);
+// console.log(listGaleryItemsEl);
+
+const backDropModal = document.querySelector(".js-lightbox");
+// console.log(backDropModal);
+
+const buttonClose = document.querySelector(".lightbox__button");
+
+const imgOnBackDrop = document.querySelector(".lightbox__image");
+// console.log(imgOnBackDrop);
+const divBackDrop = document.querySelector(".lightbox__overlay");
+// console.log(divBackDrop);
 
 const marcupHTML = createMarcupGallery(galleryData);
-console.log(marcupHTML);
-
 listGaleryItemsEl.insertAdjacentHTML("beforeend", marcupHTML);
+
+listGaleryItemsEl.addEventListener("click", onGalleryListClick);
+
+buttonClose.addEventListener("click", onCloseModalW);
+
+window.addEventListener("keydown", closeModalOnPressEsc);
+
+divBackDrop.addEventListener("click", onCloseModalW);
 
 // создал разметку
 function createMarcupGallery(params) {
   return params
-    .map(({ preview, original, description }) => {
+    .map(({ preview, original, description }, inx) => {
       return ` <li li class="gallery__item" >
     <a
         class="gallery__link"
@@ -23,6 +44,7 @@ function createMarcupGallery(params) {
             class="gallery__image"
             src="${preview}"
             data-source="${original}"
+            data-index="${inx}"
             alt="${description}"
         />
     </a>
@@ -30,5 +52,56 @@ function createMarcupGallery(params) {
     })
     .join("");
 }
+function onGalleryListClick(evt) {
+  evt.preventDefault();
+  //   console.log(evt.target.nodeName);
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  const pictureElement = evt.target;
+  const parentPictureElement = pictureElement.closest(".gallery__link");
+  currentIndex = pictureElement.dataset.index;
+  //   console.log(currentIndex);
+  onClickOpenModal();
+  createImgOnBackDrop(parentPictureElement.href, pictureElement.alt);
+}
+function onClickOpenModal(evt) {
+  backDropModal.classList.add("is-open");
+}
+function onCloseModalW(evt) {
+  backDropModal.classList.remove("is-open");
+  imgOnBackDrop.src = "";
+  imgOnBackDrop.alt = "";
+  //   closeModalOfEsc();
+}
+function createImgOnBackDrop(src, alt) {
+  imgOnBackDrop.src = src;
+  imgOnBackDrop.alt = alt;
+}
+function closeModalOnPressEsc(evt) {
+  //   console.log(evt.key);
 
-// Реализация делегирования на галерее ul.js - gallery и получение url большого изображения.
+  if (evt.key !== "Escape") {
+    slider(evt);
+    return;
+  }
+
+  onCloseModalW();
+}
+
+function slider(key) {
+  //   if (!backDropModal.classList.add("is-open")) {
+  //     return;
+  //   }
+  if (key.key === "ArrowRight") {
+    console.log(`нажал вправо`);
+    // currentIndex += 1;
+    // imgOnBackDrop.src = src;
+    // imgOnBackDrop.alt = alt;
+  } else if (key.key === "ArrowLeft") {
+    console.log(`нажал влево`);
+    // currentIndex -= 1;
+    // imgOnBackDrop.src = src;
+    // imgOnBackDrop.alt = alt;
+  }
+}
